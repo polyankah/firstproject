0ceca6528252aab99ff5f18b421c2765d688e375
polina <77857296+polyankah@users.noreply.github.com>
2021-10-10 15:55:32 +0300

    Add files via upload

---
+++ firstproject/src/test/java/pimenova/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pimenova.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
